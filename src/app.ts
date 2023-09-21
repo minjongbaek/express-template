620bdfbb74c9de7acfaae53e60b28b7e2201cf0a
Shaw <minjongbaek@gmail.com>
2023-09-21 16:15:32 +0900

    feat: add some middlewares

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import morgan from 'morgan';
 import { CREDENTIALS, LOG_FORMAT, ORIGIN } from './config';
 import { stream } from './utils/logger';
 import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
 
 export class App {
   private app: Application;
@@ -25,5 +27,9 @@ export class App {
   private initializeMiddlewares() {
     this.app.use(morgan(LOG_FORMAT, { stream }));
     this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
+    this.app.use(helmet());
+    this.app.use(compression());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
   }
 }
